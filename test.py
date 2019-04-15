f6c4b42e82b79b2896e85572889959e9fea46ff8
rabbitism <popmessiah@126.com>
2019-04-15 13:10:46 +0800

    try unit test

--- test.py
+++ test.py
@@ -1,7 +1,16 @@
-import video_processing
+import unittest
 
-import os
+import text_processing
 
-audio_dir = os.sep.join([".", "test", "test.mp3"])
-a = video_processing.get_audio_duration(audio_dir)
-print(a)
+class TestText(unittest.TestCase):
+    def test_find_suffix(self):
+        self.assertEqual('.jpg', text_processing.find_image_suffix('1.jpg'))
+        self.assertEqual('.jpg', text_processing.find_image_suffix('1.1.jpg'))
+    
+    def test_is_alpha(self):
+        self.assertTrue(text_processing.is_alpha("Hello"))
+        self.assertFalse(text_processing.is_alpha("12Hello"))
+        self.assertFalse(text_processing.is_alpha("是的"))
+
+if __name__ == "__main__":
+    unittest.main()
